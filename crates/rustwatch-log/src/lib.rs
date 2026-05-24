ec425e5591aad8c7c72c4deec488b1171749d717
selcukcukur <selcukcukur@outlook.com.tr>
2026-05-24 14:22:23 +0300

    refactor(log): unify api exports and re-export Logger and LoggerComponent

--- crates/rustwatch-log/src/lib.rs
+++ crates/rustwatch-log/src/lib.rs
@@ -1,10 +1,11 @@
 mod level;
 mod record;
+mod logger;
 
-pub mod logger;
 pub mod formatter;
 pub mod handler;
 pub mod processor;
 
+pub use logger::{Logger, LoggerComponent};
 pub use record::Record;
 pub use level::Level;
